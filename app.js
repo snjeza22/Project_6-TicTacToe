048acee60fcdd3a7897fb6b917d742b0cc4a2fa1
Snjezana Jaksic <snjezana.jaksic.sj@gmail.com>
2022-11-09 12:50:12 -0500

    added Elements

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+
+const elStatus = document.getElementById("status");
+const f11 = document.getElementById("f11");
+const f12 = document.getElementById("f12");
+const f13 = document.getElementById("f13");
+const f21 = document.getElementById("f21");
+const f22 = document.getElementById("f22");
+const f23 = document.getElementById("f23");
+const f31 = document.getElementById("f31");
+const f32 = document.getElementById("f32");
+const f33 = document.getElementById("f33");
+
 //game state
 let gameState = {};
 
